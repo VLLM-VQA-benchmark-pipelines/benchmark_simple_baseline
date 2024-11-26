1912dd325414ba3fdd8549ce27bbb069cd95d7fd
Anton Shiryaev <med.phisiker@gmail.com>
2024-11-27 07:59:02 +1000

    feat(model): Написал класс для моделей qwen2-vl
    Написал класс для моделей qwen2-vl

---
+++ src/model_utils.py
@@ -0,0 +1,66 @@
+import torch
+from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
+from qwen_vl_utils import process_vision_info
+
+
+class Qwen2VL_model:
+    def __init__(self, cache_directory):
+        # default: Load the model on the available device(s)
+        self.model = Qwen2VLForConditionalGeneration.from_pretrained(
+            "Qwen/Qwen2-VL-2B-Instruct",
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            attn_implementation="flash_attention_2",
+            cache_dir=cache_directory,
+        )
+
+        # default processor
+        self.processor = AutoProcessor.from_pretrained(
+            "Qwen/Qwen2-VL-2B-Instruct", cache_dir=cache_directory
+        )
+
+    @staticmethod
+    def get_message(image, question):
+        message = {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": image,
+                },
+                {"type": "text", "text": question},
+            ],
+        }
+
+        return message
+
+    def predict(self, image, question):
+        messages = [self.get_message(image, question)]
+
+        # Preparation for inference
+        text = self.processor.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        image_inputs, video_inputs = process_vision_info(messages)
+        inputs = self.processor(
+            text=[text],
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        )
+        inputs = inputs.to("cuda")
+
+        # Inference: Generation of the output
+        generated_ids = self.model.generate(**inputs, max_new_tokens=512)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :]
+            for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = self.processor.batch_decode(
+            generated_ids_trimmed,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )
+
+        return output_text[0]
